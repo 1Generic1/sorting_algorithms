ab2f5d703b10ff74a17674e395d0bba0c5939793
kelvin <kelvin4stunt@gmail.com>
2023-09-26 01:44:12 +0100

    sorting_algrithem

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,38 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- *                       using the Insertion sort algorithm.
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *			in ascending order
+ *			using the Insertion sort algorithm.
  *
  * @list: Pointer to a pointer to the head of the list.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *prev;
-	listint_t *next;
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	listint_t *current, *temp;
+
+	if (!list)
 		return;
-	current = (*list)->next;
+	current = *list;
 	while (current)
 	{
-		next = current->next;
-		prev = current->prev;
-		while (prev && prev->n > current->n)
+		while (current->next && (current->n > current->next->n))
 		{
-			prev = prev->prev;
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+			if (current->prev)
+				current->prev->next = temp;
+			if (temp->next)
+				temp->next->prev = current;
+			current->prev = temp;
+			temp->next = current;
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
+			print_list(*list);
 		}
-		if (prev)
-		{
-			current->prev->next = next;
-			if (next)
-				next->prev = current->prev;
-			current->prev = prev;
-			current->next = prev->next;
-			if (prev->next)
-				prev->next->prev = current;
-			prev->next = current;
-		}
-		else
-		{
-			current->next = (*list);
-			current->prev = NULL;
-			(*list)->prev = current;
-			(*list) = current;
-		}
-		print_list(*list);
-		current = next;
+		current = current->next;
 	}
 }
